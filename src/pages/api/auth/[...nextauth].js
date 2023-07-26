bb1e50395e29eb57fca791fde20f677498095e2b
pragesh19 <97692845+aeghprs@users.noreply.github.com>
2023-07-26 21:43:09 +0545

    Add google, github and facebook oauth2 authentication

---
+++ src/pages/api/auth/[...nextauth].js
@@ -0,0 +1,28 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import GithubProvider from "next-auth/providers/github"
+import FacebookProvider from "next-auth/providers/facebook"
+
+export default NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code"
+        }
+      }
+    }),
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_ID,
+      clientSecret: process.env.FACEBOOK_SECRET,
+    }),
+  ],
+});
